5ede97725b0963ca0bef0dac04f26b0f2bad46cf
Ayaz Somani (Aluance Digital) <ayazs@aluance.io>
2022-01-28 17:27:11 -0500

    Added to config

--- player/internal/config/config.go
+++ player/internal/config/config.go
@@ -5,13 +5,16 @@ import (
 	"io/fs"
 )
 
-const CONFIG_AWS_REGION = "ca-central-1"
+const (
+	CONFIG_AWS_REGION = "ca-central-1"
 
-const CONFIG_BOT_THROTTLE = 30 // ms of sleep between each game
-const CONFIG_DICTIONARY_FILENAME = "corncob_lowercase.txt"
-const CONFIG_DICTIONARY_FILEPATH = "data/" + CONFIG_DICTIONARY_FILENAME
-const CONFIG_GAME_WORDLENGTH = 5
-const CONFIG_SERVER_URL = "http://localhost:8080"
+	CONFIG_BOT_THROTTLE                            = 50 // ms of sleep between each game
+	CONFIG_DICTIONARY_FILENAME                     = "corncob_lowercase.txt"
+	CONFIG_DICTIONARY_FILEPATH                     = "data/" + CONFIG_DICTIONARY_FILENAME
+	CONFIG_DICTIONARY_GENERATE_REAL_WORD_INJECTION = 0.3
+	CONFIG_GAME_WORDLENGTH                         = 5
+	CONFIG_SERVER_URL                              = "http://localhost:8080"
+)
 
 //go:embed data/*
 var embFS embed.FS
